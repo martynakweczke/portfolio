6644f6e9914e2209d1c4c9da0c000b518712b341
Martyna Kweczke <martynakweczke@gmail.com>
2026-09-07 22:58:58 +0200

    remove unnecessary config values

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -2,12 +2,8 @@ import type { Lang } from "@/lib/content";
 
 type SiteConfig = {
   defaultLang: Lang;
-  showPhoto: boolean;
-  showBeforeTech: boolean;
 };
 
 export const siteConfig: SiteConfig = {
   defaultLang: "en",
-  showPhoto: true,
-  showBeforeTech: true,
 };
